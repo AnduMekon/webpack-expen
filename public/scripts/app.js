ba0f3564f06b7224acb666298300467f7a071442
AnduMekon <anduabe@yahoo.com>
2020-01-22 14:19:50 -0500

    first commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,16 +3,85 @@
 console.log('app.js is running');
 
 // JSX - JavaScript XML 
-var template = React.createElement(
-  P,
-  null,
-  'This JSX from app.js! '
-);
+
+var numbers = [55, 11, 200, 190];
+
+var app = {
+    title: 'Indecision Andualem',
+    subtitle: 'Put your life on the hands of a computer',
+    options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
+};
+
 var appRoot = document.getElementById('app');
 
-// I love jesus 2
-//I love jesus 3
-// i love jesus 4
-//i love jesus github
-
-ReactDOM.render(template, appRoot);
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your option123' : 'No option'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add your comment'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+render();
--- src/app.js
+++ src/app.js
@@ -1,12 +1,57 @@
 console.log('app.js is running');
 
 // JSX - JavaScript XML 
-var template =  <P>This JSX from app.js! </P>;
+
+ const numbers = [55, 11, 200, 190];                           
+
+const app = {
+    title: 'Indecision Andualem',
+    subtitle: 'Put your life on the hands of a computer',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option){
+    app.options.push(option);
+    e.target.elements.option.value='';
+    render();
+    }
+};
+const onRemoveAll = () => {
+    app.options =[];
+    render();
+};
+
+
 var appRoot = document.getElementById('app');
 
-// I love jesus 2
-//I love jesus 3
-// i love jesus 4
-//i love jesus github
+const render = () => {
+    const template = (
+        <div>
+         <h1>{app.title}</h1>
+         {app.subtitle && <p>{app.subtitle}</p>}
+         <p>{app.options.length > 0 ? 'Here are your option123' : 'No option'}</p>
+         <p>{app.options.length}</p>
+         <button onClick={onRemoveAll}>Remove All</button>
+         { 
+         }
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}> 
+                <input type="text" name ="option"/>
+                <button>Add your comment</button>
+            </form>
+        
+        </div>
+    )
+    ReactDOM.render(template, appRoot);
+};
+render();
 
-ReactDOM.render(template, appRoot)
+
+
+
