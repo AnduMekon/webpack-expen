72b9ccb259514854989f2915170cd54ee9f883cf
AnduMekon <anduabe@yahoo.com>
2020-01-29 18:29:09 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,41 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-let count = 0; 
-const addOne = () => {
-    count++;
-    renderCounterApp();
+import TaskApp from './components/taskApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
- 
-}
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-const reset = () => {
-    count = 0;
- renderCounterApp();
-}
-
-var appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
- var templateTwo = (
-     <div>
-     <h1>Count: {count}</h1>
-     <button onClick={addOne}> +1</button>
-     <button onClick={minusOne}> -1</button>
-     <button onClick={reset}> reset</button>
- 
-     
- 
-     </div>
- 
- );
-ReactDOM.render(templateTwo, appRoot)
-};
-renderCounterApp();
-
-
-
-
+ReactDOM.render(<TaskApp />, document.getElementById('app'));
