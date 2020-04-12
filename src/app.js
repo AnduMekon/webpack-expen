1364ef85946c155635ed3671ad38ea6657c0b771
AnduMekon <anduabe@yahoo.com>
2020-04-12 13:18:47 -0400

    cking

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TaskApp from './components/taskApp';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<TaskApp />, document.getElementById('app'));
+class Ck extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1> this is ck</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(< Ck />, document.getElementById('app'));
